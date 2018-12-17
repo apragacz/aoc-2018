af334b6f6cdb7b95dee821b770cc2c5aa9fbe59b
Andrzej Pragacz <apragacz@o2.pl>
2018-12-17 01:54:46 +0100

    Added run.sh script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euox pipefail
+
+main() {
+    local src_filename="$1"
+    local bin_filename
+    bin_filename="${src_filename%.*}.bin"
+    rustc "${src_filename}" -o "${bin_filename}"
+    "./${bin_filename}"
+}
+
+main "$@"
