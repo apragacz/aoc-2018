176ade98f84d8ebb9e8bbb5c0d174055889b8480
Andrzej Pragacz <apragacz@o2.pl>
2018-12-18 01:15:14 +0100

    run.sh passes args

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ set -euox pipefail
 main() {
     local src_filename="$1"
     local bin_filename
+    shift
     bin_filename="${src_filename%.*}.bin"
     rustc "${src_filename}" -o "${bin_filename}"
-    "./${bin_filename}"
+    "./${bin_filename}" "$@"
 }
 
 main "$@"
